9477b864c05b7deda2d79511989200085f07efe6
Minioin <minioin@protonmail.com>
2020-04-16 08:00:39 +0545

    Add from implementation for &[u8] and &str, remove for Url

--- src/sha2.rs
+++ src/sha2.rs
@@ -1,29 +1,32 @@
 use crate::TinyId;
 use sha2::{Digest, Sha256};
-use url::Url;
 
-impl From<Url> for TinyId {
-    fn from(url: Url) -> Self {
+impl From<&[u8]> for TinyId {
+    fn from(input: &[u8]) -> Self {
         let mut hasher = Sha256::new();
-        hasher.input(url.as_str());
+        hasher.input(input);
         let result = hasher.result();
         Self(to_u64(result.as_slice().split_at(8).0))
     }
 }
 
+impl From<&str> for TinyId {
+    fn from(input: &str) -> Self {
+        Self::from(input.as_bytes())
+    }
+}
+
 fn to_u64(values: &[u8]) -> u64 {
-    println!("{}", values.len());
-    values.iter().fold(0, |x, &i| x << 8 | i as u64)
+    values.iter().take(8).fold(0, |x, &i| x << 8 | i as u64)
 }
 
 mod test {
     use crate::TinyId;
-    use url::Url;
 
     #[test]
 
     fn test() {
-        let url: Url = "https://dbhattarai.info.np/".parse::<Url>().unwrap();
+        let url = "https://dbhattarai.info.np/";
         let id: TinyId = url.into();
         assert_eq!(format!("{}", id), "QMlX0WD1Fmg=");
     }
